5d552a28cb97bfd1fee640f45c4a28cef797f27e
Pe6e3 <xpe6e3@gmail.com>
2023-05-24 13:23:01 +0600

    Скорректировал тип объекта Slug (int->string)

--- Portal.DLL/Entities/Category.cs
+++ Portal.DLL/Entities/Category.cs
@@ -4,7 +4,7 @@ public class Category : BaseEntity
 {
     public string? Name { get; set; }
     public string? Description { get; set; }
-    public int Slug { get; set; }
+    public string? Slug { get; set; }
     public string? CategoryImage { get; set; }
 
     public List<Post>? Posts { get; } = new();
