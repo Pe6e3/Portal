01d75258f4924330efc75df2b5f0030c4e9b428e
Pe6e3 <xpe6e3@gmail.com>
2023-06-01 21:02:25 +0600

    Немного оформил ленту новостей Пользователя

--- Portal.Web/Controllers/HomeController.cs
+++ Portal.Web/Controllers/HomeController.cs
@@ -1,21 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portal.BLL;
+using Portal.DAL.Entities;
+using Portal.DAL.Interfaces;
+using Portal.Web.Areas.Admin.Controllers;
 using Portal.Web.Models;
+using Portal.Web.ViewModels;
 using System.Diagnostics;
 
 namespace Portal.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController<Post, IPostRepository>
     {
-        private readonly ILogger<HomeController> _logger;
+        protected new readonly ILogger<BaseController<Post, IPostRepository>> _logger;
+        private readonly UnitOfWork _uow;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(UnitOfWork unitOfWork, ILogger<BaseController<Post, IPostRepository>> logger, IPostRepository repository)
+            : base(unitOfWork, logger, repository)
         {
             _logger = logger;
+            _uow = unitOfWork;
         }
 
-        public IActionResult Index()
+        public override async Task<IActionResult> Index()
         {
-            return View();
+            List<Post> allPosts = (List<Post>)await _uow.PostRep.ListAllAsync();
+            List<PostContent> allContent = (List<PostContent>)await _uow.PostContentRep.ListAllAsync();
+            List<PostViewModel> posts = new List<PostViewModel>();
+
+            foreach (Post post in allPosts)
+            {
+                PostContent? content = allContent.FirstOrDefault(x => x.PostId == post.Id);
+                posts.Add(new PostViewModel()
+                {
+                    Slug = post.Slug,
+                    CreatedAt = post.CreatedAt,
+                    PostId = post.Id,
+                    Title = content.Title,
+                    PostBody = content.PostBody,
+                    PostImage = content.PostImage,
+                    PostVideo = content.PostVideo,
+                    CommentsClosed = content.CommentsClosed
+                });
+            }
+            return View(posts);
         }
 
         public IActionResult Privacy()
