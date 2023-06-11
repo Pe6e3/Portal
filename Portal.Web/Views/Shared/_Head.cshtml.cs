54d3422a9a6e0ad0106320b1e1f3693cd19d9277
Pe6e3 <xpe6e3@gmail.com>
2023-06-11 14:46:07 +0600

    Разбил Layout`ы на частичные представления

---
+++ Portal.Web/Views/Shared/_Head.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portal.Web.Views.Shared
+{
+    public class _HeadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
