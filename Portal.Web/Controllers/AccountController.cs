c9b4d8fa1ac43e57318e1d1d9875158ee95cb703
Pe6e3 <xpe6e3@gmail.com>
2023-06-15 22:02:12 +0600

    Начал создавать авторизацию

---
+++ Portal.Web/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portal.DAL.Entities;
+using Portal.Web.ViewModels;
+
+namespace Portal.Web.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Register(LoginViewModel lvm)
+		{
+			return View();
+		}
+
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Login(LoginViewModel lvm)
+		{
+			return View();
+		}
+
+
+		public async Task<IActionResult> Authenticate(User user)
+		{
+
+			return View();
+		}
+
+
+		public IActionResult Logout()
+		{
+			return View();
+		}
+
+
+
+
+
+	}
+}
---
+++ Portal.Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portal.Web.ViewModels;
+
+public class LoginViewModel
+{
+
+// User
+	[Display(Name = "Логин")]
+	[Required(ErrorMessage = "Не указан логин")]
+	public string Login { get; set; } = null!;
+
+	[Required(ErrorMessage = "Не указан Email")]
+	[DataType(DataType.EmailAddress)]
+	public string? Email { get; set; }
+
+	[Required(ErrorMessage = "Не указан пароль")]
+	[DataType(DataType.Password)]
+
+
+
+
+// UserProfile 
+	public string? Password { get; set; }
+	[Display(Name = "Имя")]
+	public string? Firstname { get; set; }
+	[Display(Name = "Фамилия")]
+	public string? Lastname { get; set; }
+
+}
