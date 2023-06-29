8541db631cf0c4141fa6c6cb4a5f9436f51b0eb2
Pe6e3 <xpe6e3@gmail.com>
2023-06-30 01:23:11 +0600

    Рефакторинг репозиториев

--- Portal.BLL/Repositories/PostRepository.cs
+++ Portal.BLL/Repositories/PostRepository.cs
@@ -14,9 +14,12 @@ public class PostRepository : GenericRepositoryAsync<Post>, IPostRepository
         this.db = db;
     }
 
-    public async Task<Post> GetPostByIdAsync(int id) => await db.Posts.Include(p => p.Content).Include(p => p.CreatedBy).FirstOrDefaultAsync(x => x.Id == id);
+    //public async Task<Post> GetPostByIdAsync(int id) => await db.Posts.Include(p => p.Content).Include(p => p.CreatedBy).FirstOrDefaultAsync(x => x.Id == id);
+    public async Task<Post> GetPostByIdAsync(int id) => await db.Posts.FirstOrDefaultAsync(x => x.Id == id);
+    public async Task<Post> GetPostByIdAsync(int id, string include1) => await db.Posts.Include(include1).FirstOrDefaultAsync(x => x.Id == id);
+    public async Task<Post> GetPostByIdAsync(int id, string include1, string include2) => await db.Posts.Include(include1).Include(include2).FirstOrDefaultAsync(x => x.Id == id);
 
-    public async Task<Post?> GetPostBySlugAsync(string postSlug) => await db.Posts.Include(x=>x.Categories).Include(x=>x.Content).FirstOrDefaultAsync(post => post.Slug == postSlug);
+    public async Task<Post?> GetPostBySlugAsync(string postSlug) => await db.Posts.Include(x => x.Categories).Include(x => x.Content).FirstOrDefaultAsync(post => post.Slug == postSlug);
 
 
 
