f173275b9847f7f1b1ed6fd02745d4e2fea00149
Pe6e3 <xpe6e3@gmail.com>
2023-07-01 04:16:20 +0600

    Частичное представление для 5 постов справа в представлении отдельного Поста

---
+++ Portal.Web/Views/Shared/Components/FiveLastPostsColumnViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portal.BLL;
+using Portal.DAL.Entities;
+
+namespace Portal.Web.Views.Shared.Components;
+
+[ViewComponent(Name = "_FiveLastPostsColumn")]
+public class FiveLastPostsColumnViewComponent : ViewComponent
+{
+    private readonly UnitOfWork uow;
+    public FiveLastPostsColumnViewComponent(UnitOfWork uow)
+    {
+        this.uow = uow;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync() => 
+        View(await uow.PostRep.ListAllAsync(5, "Content","Comments"));
+
+}
