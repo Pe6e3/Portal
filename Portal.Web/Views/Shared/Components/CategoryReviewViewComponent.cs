c8e2895b9584034a726f73be3c8c36729971e647
Pe6e3 <xpe6e3@gmail.com>
2023-07-02 13:50:20 +0600

    Начал делать отдельное представление для обзора категории

---
+++ Portal.Web/Views/Shared/Components/CategoryReviewViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portal.BLL;
+using Portal.DAL.Entities;
+
+namespace Portal.Web.Views.Shared.Components
+{
+    [ViewComponent(Name = "_TopMenu")]
+    public class CategoryReviewViewComponent : ViewComponent
+    {
+        private readonly UnitOfWork uow;
+
+        public CategoryReviewViewComponent(UnitOfWork uow)
+        {
+            this.uow = uow;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()  
+        {
+
+            return View( );
+        }
+
+    }
+}
