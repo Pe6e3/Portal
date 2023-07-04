4c3fe4c90a215f901a6e68da6829d99cabfcd48f
Pe6e3 <xpe6e3@gmail.com>
2023-07-04 19:09:47 +0600

    Создал контроллер для Комментариев

---
+++ Portal.Web/Controllers/CommentsController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Portal.BLL;
+using Portal.DAL.Entities;
+using Portal.DAL.Interfaces;
+using Portal.Web.Areas.Admin.Controllers;
+
+namespace Portal.Web.Controllers;
+
+public class CommentsController : BaseController<Comment, ICommentRepository>
+{
+    protected new readonly ILogger<BaseController<Comment, ICommentRepository>> logger;
+    private readonly UnitOfWork uow;
+    private readonly IMapper mapper;
+
+
+    public CommentsController(UnitOfWork uow, ILogger<BaseController<Comment, ICommentRepository>> logger, ICommentRepository repository, IMapper mapper)
+             : base(uow, logger, repository)
+    {
+        this.logger = logger;
+        this.mapper = mapper;
+        this.uow = uow;
+    }
+
+ 
+}
