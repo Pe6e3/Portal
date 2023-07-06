887ed5002a459ac2f8bdaee5eec11dbfbdb87789
Pe6e3 <xpe6e3@gmail.com>
2023-07-06 15:54:55 +0600

    Создал сущность и частичное представление MyLogger

---
+++ Portal.DLL/Entities/MyLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.DAL.Entities;
+public class MyLogger : BaseEntity
+{
+    public DateTime Date { get; set; }
+    public int UserId { get; set; }
+    public string? UserIP { get; set; }
+    public string? userLocation { get; set; }
+    public string? UserClick { get; set; }
+    public virtual User? User { get; set; }
+
+}
+
