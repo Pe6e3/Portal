114324290c4aa9f1c0bf7c8f554d1e119270630a
Anton <xpe6e3@gmail.com>
2023-09-05 13:45:28 +0600

    Добавил в логгирование пользователей их Ip, страну и город

--- Portal.Web/Middleware/LogUserActionMiddleware.cs
+++ Portal.Web/Middleware/LogUserActionMiddleware.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.CodeAnalysis.Differencing;
 using Portal.BLL;
 using Portal.DAL.Entities;
+using System.Net;
+using System.Text.RegularExpressions;
 
 public class LogUserActionMiddleware : IMiddleware
 {
@@ -22,6 +25,27 @@ public class LogUserActionMiddleware : IMiddleware
             logger.UserClick = context.Request.Path.Value;
             logger.UserId = user != null ? user.Id : await uow.UserRep.GetDefaultUserId();
             logger.Date = DateTime.Now;
+
+            string lineApiIp = "";
+            string lineApiLocation = "";
+            string regexIp = "\"ip\":\"(.*?)\"";
+            string regexCountry = "\"country\":\"(.*?)\"";
+            string regexCity = "\"city\":\"(.*?)\"";
+
+
+
+            using (WebClient wc = new WebClient())
+            {
+                lineApiIp = wc.DownloadString("http://ipwho.is/");
+                Match matchIp = Regex.Match(lineApiIp, regexIp);
+                logger.UserIP = matchIp.Groups[1].Value;
+
+                lineApiLocation = wc.DownloadString("http://ipwho.is/" + logger.UserIP);
+            }
+
+            Match matchCountry = Regex.Match(lineApiLocation, regexCountry);
+            Match matchCity = Regex.Match(lineApiLocation, regexCity);
+            logger.UserLocation = matchCountry.Groups[1].Value + " - " + matchCity.Groups[1].Value;
             await uow.MyLoggerRep.InsertAsync(logger);
         }
 
